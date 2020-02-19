d93421132fd038b28aa8ce00677afa0743dc9cb3
dsperry-ape09 <daniel.sperry@gmail.com>
2020-02-19 16:23:39 +0000

    Implemented Cache Expiration

--- src/main/java/com/develogical/TemperatureCache.java
+++ src/main/java/com/develogical/TemperatureCache.java
@@ -2,29 +2,39 @@ package com.develogical;
 
 import javafx.util.Pair;
 
+import java.time.Clock;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class TemperatureCache implements TemperatureRetriever {
     private TemperatureRetriever source;
     private int cacheSize;
+    private Clock clock;
 
-    private Map<Pair<String, String>, Double> temperatureCacheMap = new LinkedHashMap<>();
+    private Map<Pair<String, String>, Entry> temperatureCacheMap = new LinkedHashMap<>();
 
-    public TemperatureCache(TemperatureRetriever source, int cacheSize) {
+    public TemperatureCache(TemperatureRetriever source, int cacheSize, Clock clock) {
         this.source = source;
         this.cacheSize = cacheSize;
+        this.clock = clock;
     }
 
     @Override
     public double getTemperature(String region, String day) {
         Pair<String, String> key = new Pair<>(region, day);
-        Double cachedTemperature = temperatureCacheMap.get(key);
-        if (cachedTemperature != null) {
-            return cachedTemperature;
+        Entry entry = temperatureCacheMap.get(key);
+        if (entry != null) {
+            if (clock.millis() - entry.timestamp < TimeUnit.DAYS.toMillis(1)) {
+                return entry.temperature;
+            }
+            temperatureCacheMap.remove(key);
         }
         double sourceTemperature = source.getTemperature(region, day);
-        temperatureCacheMap.put(key, sourceTemperature);
+        entry = new Entry();
+        entry.temperature = sourceTemperature;
+        entry.timestamp = clock.millis();
+        temperatureCacheMap.put(key, entry);
         enforceCacheSize();
         return sourceTemperature;
     }
@@ -34,5 +44,9 @@ public class TemperatureCache implements TemperatureRetriever {
             Pair<String, String> oldestKey = temperatureCacheMap.entrySet().iterator().next().getKey();
             temperatureCacheMap.remove(oldestKey);
         }
+    }
+    private class Entry{
+        long timestamp;
+        double temperature;
     }
 }
--- src/test/java/com/develogical/CacheTest.java
+++ src/test/java/com/develogical/CacheTest.java
@@ -1,6 +1,9 @@
 package com.develogical;
 
 import org.junit.Test;
+import java.time.Clock;
+import java.util.concurrent.TimeUnit;
+
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -11,7 +14,7 @@ public class CacheTest {
     @Test
     public void simpleRetrieval() throws Exception {
         TemperatureRetriever delegate = mock(TemperatureRetriever.class);
-        TemperatureRetriever cache = new TemperatureCache(delegate, 3);
+        TemperatureRetriever cache = new TemperatureCache(delegate, 3, Clock.systemUTC());
         when(delegate.getTemperature(any(String.class), any(String.class))).thenReturn(2.0);
         assertThat(cache.getTemperature("LONDON", "MONDAY"), equalTo(2.0));
     }
@@ -19,7 +22,7 @@ public class CacheTest {
     @Test
     public void cacheHit() throws Exception {
         TemperatureRetriever delegate = mock(TemperatureRetriever.class);
-        TemperatureRetriever cache = new TemperatureCache(delegate, 3);
+        TemperatureRetriever cache = new TemperatureCache(delegate, 3, Clock.systemUTC());
         when(delegate.getTemperature(any(String.class), any(String.class))).thenReturn(2.0);
         cache.getTemperature("LONDON", "MONDAY");
         cache.getTemperature("LONDON", "MONDAY");
@@ -29,12 +32,24 @@ public class CacheTest {
     @Test
     public void cacheLimitExceeded() throws Exception {
         TemperatureRetriever delegate = mock(TemperatureRetriever.class);
-        TemperatureRetriever cache = new TemperatureCache(delegate, 1);
+        TemperatureRetriever cache = new TemperatureCache(delegate, 1, Clock.systemUTC());
         when(delegate.getTemperature(any(String.class), any(String.class))).thenReturn(2.0);
         cache.getTemperature("LONDON", "MONDAY");
         cache.getTemperature("LONDON", "TUESDAY");
         cache.getTemperature("LONDON", "MONDAY");
         verify(delegate, times(3)).getTemperature(any(String.class), any(String.class));
+    }
+    @Test
+    public void cacheExpirationTest() throws Exception {
+        TemperatureRetriever delegate = mock(TemperatureRetriever.class);
+        Clock clock = mock(Clock.class);
+        TemperatureRetriever cache = new TemperatureCache(delegate, 1, clock);
+        when(delegate.getTemperature(any(String.class), any(String.class))).thenReturn(2.0);
+        when(clock.millis()).thenReturn(0L);
+        cache.getTemperature("LONDON", "MONDAY");
+        when(clock.millis()).thenReturn(TimeUnit.DAYS.toMillis(1));
+        cache.getTemperature("LONDON", "MONDAY");
+        verify(delegate, times(2)).getTemperature(any(String.class), any(String.class));
     }
 
 }
