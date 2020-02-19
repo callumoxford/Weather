65973afa74b4d1c1a8882ef36faaf7244a7199b3
dsperry-ape09 <daniel.sperry@gmail.com>
2020-02-19 15:55:06 +0000

    Limiting the cache size.

--- src/main/java/com/develogical/TemperatureCache.java
+++ src/main/java/com/develogical/TemperatureCache.java
@@ -7,11 +7,13 @@ import java.util.Map;
 
 public class TemperatureCache implements TemperatureRetriever {
     private TemperatureRetriever source;
+    private int cacheSize;
 
     private Map<Pair<String, String>, Double> temperatureCacheMap = new LinkedHashMap<>();
 
-    public TemperatureCache(TemperatureRetriever source) {
+    public TemperatureCache(TemperatureRetriever source, int cacheSize) {
         this.source = source;
+        this.cacheSize = cacheSize;
     }
 
     @Override
@@ -23,6 +25,14 @@ public class TemperatureCache implements TemperatureRetriever {
         }
         double sourceTemperature = source.getTemperature(region, day);
         temperatureCacheMap.put(key, sourceTemperature);
+        enforceCacheSize();
         return sourceTemperature;
+    }
+
+    private void enforceCacheSize() {
+        while (temperatureCacheMap.size() > cacheSize) {
+            Pair<String, String> oldestKey = temperatureCacheMap.entrySet().iterator().next().getKey();
+            temperatureCacheMap.remove(oldestKey);
+        }
     }
 }
--- src/test/java/com/develogical/CacheTest.java
+++ src/test/java/com/develogical/CacheTest.java
@@ -11,7 +11,7 @@ public class CacheTest {
     @Test
     public void simpleRetrieval() throws Exception {
         TemperatureRetriever delegate = mock(TemperatureRetriever.class);
-        TemperatureRetriever cache = new TemperatureCache(delegate);
+        TemperatureRetriever cache = new TemperatureCache(delegate, 3);
         when(delegate.getTemperature(any(String.class), any(String.class))).thenReturn(2.0);
         assertThat(cache.getTemperature("LONDON", "MONDAY"), equalTo(2.0));
     }
@@ -19,10 +19,22 @@ public class CacheTest {
     @Test
     public void cacheHit() throws Exception {
         TemperatureRetriever delegate = mock(TemperatureRetriever.class);
-        TemperatureRetriever cache = new TemperatureCache(delegate);
+        TemperatureRetriever cache = new TemperatureCache(delegate, 3);
         when(delegate.getTemperature(any(String.class), any(String.class))).thenReturn(2.0);
         cache.getTemperature("LONDON", "MONDAY");
         cache.getTemperature("LONDON", "MONDAY");
         verify(delegate, times(1)).getTemperature(any(String.class), any(String.class));
     }
+
+    @Test
+    public void cacheLimitExceeded() throws Exception {
+        TemperatureRetriever delegate = mock(TemperatureRetriever.class);
+        TemperatureRetriever cache = new TemperatureCache(delegate, 1);
+        when(delegate.getTemperature(any(String.class), any(String.class))).thenReturn(2.0);
+        cache.getTemperature("LONDON", "MONDAY");
+        cache.getTemperature("LONDON", "TUESDAY");
+        cache.getTemperature("LONDON", "MONDAY");
+        verify(delegate, times(3)).getTemperature(any(String.class), any(String.class));
+    }
+
 }
